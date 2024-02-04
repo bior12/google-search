cc62f86c6cf06684e6bd88b62919baa041968b39
philip saroka <philipsaroka@gmail.com>
2024-02-04 09:53:09 -0500

    sample output

--- google-search.py
+++ google-search.py
@@ -1,6 +1,15 @@
 import requests
+from dotenv import load_dotenv
+import os
+import csv
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Your API key and Custom Search Engine ID
+api_key = os.getenv("api_key")
+cse_id = os.getenv("cse_id")
 
 
 # Query to search for, including the "site:" operator
@@ -14,6 +23,10 @@ params = {
     "q": query,
 }
 
+data_table = []
+headers = ["name", "link"]
+data_table.append(headers)
+
 try:
     response = requests.get(base_url, params=params)
     data = response.json()
@@ -21,11 +34,19 @@ try:
     if "items" in data:
         search_results = data["items"]
         for idx, result in enumerate(search_results, start=1):
-            print(f"{idx}: {result['title']}")
-            print(result['link'])
-            print("\n")
+            data_table.append([result['title'], result['link']])
     else:
         print("No search results found.")
 
 except Exception as e:
     print(f"Error: {e}")
+
+with open('output.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    # Write the header row
+    csvwriter.writerow(data_table[0])
+
+    # Write the data rows
+    csvwriter.writerows(data_table[1:])
+
